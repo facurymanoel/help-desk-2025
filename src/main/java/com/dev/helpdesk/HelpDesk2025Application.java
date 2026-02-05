1faa6b5d7e467052a68e9bb24ecb6cca24951844
facurymanoel <mffacuri@terra.com.br>
2026-02-05 15:50:10 -0300

    Primeiro commit

---
+++ src/main/java/com/dev/helpdesk/HelpDesk2025Application.java
@@ -0,0 +1,13 @@
+package com.dev.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDesk2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDesk2025Application.class, args);
+	}
+
+}
